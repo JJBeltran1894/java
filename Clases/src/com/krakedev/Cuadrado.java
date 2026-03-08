396881047d87251129e55bec5be2793e55ac02f1
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-07 21:16:42 -0500

    Reto 10 - Clase Cuadrado metodo calcularPerimetro

--- Clases/src/com/krakedev/Cuadrado.java
+++ Clases/src/com/krakedev/Cuadrado.java
@@ -6,7 +6,11 @@ public class Cuadrado {
 	public double lado;
 	
 	public double calcularArea() {
-		double area=lado
+		double area=Math.pow(lado, 2);
 		return area;
+	}
+	public double calcularPerimetro() {
+		double perimetro=lado*4;
+		return perimetro;
 	}
 }
