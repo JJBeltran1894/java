495bd9057782f1b79440ccaad9935b2a219f3a55
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-07 20:24:23 -0500

    Reto 9 - Metodos llamados desde la clase TestCalculadora

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -6,15 +6,23 @@ public class TestCalculadora {
 		Calculadora calcu;
 		int resultadoSuma;
 		double resultadoResta;
+		double resultadoMultiplicacion;
+		double resultadoDivision;
+		double resultadoPromedio;
 		
 		calcu= new Calculadora();
 		
 		resultadoSuma=calcu.sumar(5,8);
 		resultadoResta=calcu.restar(10,3);
-		
+		resultadoMultiplicacion=calcu.multiplicar(25, 13.25);
+		resultadoDivision=calcu.dividir(resultadoMultiplicacion, 4);		
+		resultadoPromedio=calcu.promediar(resultadoMultiplicacion, resultadoDivision, resultadoResta);
 		System.out.println("Resultado Suma: "+resultadoSuma);
 		System.out.println("Resultado Resta: "+resultadoResta);
-		
+		System.out.println("Resultado Multiplicacion: "+resultadoMultiplicacion);
+		System.out.println("Resultado Division: "+resultadoDivision);
+		System.out.println("Resultado Promedio entre: "+resultadoMultiplicacion+", "+resultadoDivision+", "+resultadoResta+" es igual a : " +resultadoPromedio);
+		calcu.mostrarResultado();
 	}
 
 }
