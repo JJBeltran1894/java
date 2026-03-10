8b76eebad1460d48399b588ed5b3f678486ecad7
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-10 01:40:24 -0500

    Reto 15 - Ejercicio 2 completado

---
+++ Repaso/src/com/cmc/repaso/test/TestEstudiante.java
@@ -0,0 +1,14 @@
+package com.cmc.repaso.test;
+
+import com.cmc.repaso.entidades.Estudiante;
+
+public class TestEstudiante {
+
+	public static void main(String[] args) {
+		Estudiante estudiante=new Estudiante("Jorge");
+		estudiante.calificar(8);
+		
+
+	}
+
+}
