208ee271114847c394d26ebbd363a42e20d54b55
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-13 01:22:07 -0500

    Reto 18 - Ejercicio 2

---
+++ EvaluacionJuanJoseBeltran/src/com/clearminds/test/TestCelda.java
@@ -0,0 +1,19 @@
+package com.clearminds.test;
+
+import com.clearminds.componentes.Celda;
+
+public class TestCelda {
+	public static void main(String[] args) {
+		Celda celda=new Celda("A1");
+		System.out.println(celda.getProducto().getNombre());//EN ESTA LINEA ES EL ERROR
+		// Error en la linea 8, El atributo producto (clase Producto) del objeto celda es null (metodo getProducto ln 20)
+		// Por ende el atributo nombre del objeto Producto tambien es null (metodo getNombre class Producto ln 25)
+		
+		System.out.println("CELDA:"+celda.getProducto());
+		System.out.println("Nombre Producto:"+celda.getProducto().getNombre());
+		System.out.println("Precio Producto:"+celda.getProducto().getPrecio());
+		System.out.println("Codigo Producto:"+celda.getProducto().getCodigo());
+		System.out.println("STOCK:"+celda.getStock());
+		System.out.println("*************************************");
+	}
+}
