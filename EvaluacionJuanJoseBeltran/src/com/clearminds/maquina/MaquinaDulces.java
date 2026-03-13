5d73f9167e6776bd911f730ca2d2915a00e2d6e9
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-13 01:53:17 -0500

    Reto 18 - Ejercicio 4

--- EvaluacionJuanJoseBeltran/src/com/clearminds/maquina/MaquinaDulces.java
+++ EvaluacionJuanJoseBeltran/src/com/clearminds/maquina/MaquinaDulces.java
@@ -8,6 +8,10 @@ public class MaquinaDulces {
 	private ArrayList<Celda> celdas;
 	private double saldo;
 	
+	public MaquinaDulces(){
+		celdas = new ArrayList<Celda>();
+	}
+	
 	public void agregarCelda(String codCelda) {
 		Celda celda= new Celda(codCelda);
 		celdas.add(celda);
@@ -16,9 +20,18 @@ public class MaquinaDulces {
 	public void mostrarConfiguracion() {
 		System.out.println("**Configuracion Maquina**");
 		for(int i=0;i<celdas.size();i++) {
-			System.out.println("  >Celda "+i+": "+celdas.get(i).getCodigo());
+			System.out.println("  >Celda "+(i+1)+": "+celdas.get(i).getCodigo());
 		}
 
 		System.out.println("-------------------------");
+	}
+	public Celda buscarCelda(String codCelda) {
+		Celda celdaEncontrada=null;
+		for(int i=0;i<celdas.size();i++) {
+			if(celdas.get(i).getCodigo()==codCelda) {
+				celdaEncontrada=celdas.get(i);
+			}
+		}
+		return celdaEncontrada;
 	}
 }
