e0a0ba90966619756abf4a3f2de47175605173dd
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-14 19:38:49 -0500

    Reto 21 - Cartas - Ejercicio 6

--- Cartas/src/com/servicios/Juego.java
+++ Cartas/src/com/servicios/Juego.java
@@ -7,15 +7,33 @@ import com.entidades.Naipe;
 
 public class Juego {
 	private Naipe naipe;
-	private ArrayList<ArrayList<Carta>> cartasJugador;
+	private ArrayList<ArrayList<Carta>> cartasJugadores;
+	private ArrayList<Carta> naipeBarajado;
 	
-	public Juego(ArrayList<String> idsJugadores ) {
+	public Juego(ArrayList<String> idsJugadores) {
 		ArrayList<Carta> arregloCartas= new ArrayList<Carta>(); 
-		cartasJugador= new ArrayList<ArrayList<Carta>>();
+		cartasJugadores= new ArrayList<ArrayList<Carta>>();
 		for(int i=0;i<idsJugadores.size();i++) {
-			cartasJugador.add(arregloCartas);
+			cartasJugadores.add(arregloCartas);
+		}
+	}
+	public Juego() {
+		naipe=new Naipe();
+		naipeBarajado=naipe.barajar();
+	}
+
+	public ArrayList<ArrayList<Carta>> getCartasJugadores() {
+		return cartasJugadores;
+		
+	}
+	public void entregarCartas(int cartasPorJugador) {
+		int ordenCarta=0;
+		for(int x=0;x<cartasPorJugador;x++) {
+			for(int i=0;i<cartasJugadores.size();i++) {
+				cartasJugadores.get(i).add(naipeBarajado.get(ordenCarta));
+				ordenCarta++;
+			}
 		}
 	}
 	
-
 }
