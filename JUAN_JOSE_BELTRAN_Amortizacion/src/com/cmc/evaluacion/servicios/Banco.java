d04f9fc99521ad42544040e225f199085e1b3099
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-15 00:58:56 -0500

    Reto 22 - Tabla de Amortizacion

---
+++ JUAN_JOSE_BELTRAN_Amortizacion/src/com/cmc/evaluacion/servicios/Banco.java
@@ -0,0 +1,11 @@
+package com.cmc.evaluacion.servicios;
+
+import java.util.ArrayList;
+
+import com.cmc.evaluacion.entidades.Cliente;
+import com.cmc.evaluacion.entidades.Prestamo;
+
+public class Banco {
+	private ArrayList<Prestamo> prestamos;
+	private ArrayList<Cliente> clientes;
+}
