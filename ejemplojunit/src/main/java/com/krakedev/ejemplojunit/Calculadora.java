164ab5c48b11cc2dde755523c75e448e3ad42bd0
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-03-17 23:34:38 -0500

    Reto 30 - carga Archivos proyecto maven - ejemplojunit y video explicativo

---
+++ ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,12 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	public int sumar(int num1,int num2) {
+		int sum = num1 + num2;
+		return sum;
+	}
+	public int restar(int num1,int num2) {
+		int rest = num1 - num2;
+		return rest;
+	}
+}
